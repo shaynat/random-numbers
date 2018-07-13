a62b35d5dd6b25703625ba05e521fa95adb8b31b
Shayna Twersky <shaynatwersky@gmail.com>
2018-07-12 22:46:43 -0400

    Initial commit

---
+++ Class 67/Scripts/index.js
@@ -0,0 +1,19 @@
+﻿new Vue({
+
+    el: '#random-numbers',
+
+    data: {
+        max: '',
+        min: '',
+        numbers: []
+    },
+
+    methods: {
+
+        addnumberClick: function () {
+
+            const num = Math.floor(Math.random() * (parseInt(this.max) - parseInt(this.min) + 1) + parseInt(this.min));
+            this.numbers.push(num);
+        }
+    }
+});
